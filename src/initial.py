f521a0d5e17b58eb8498248d94876b47deb64375
Mark Basham <mark.basham@diamond.ac.uk>
2015-04-29 09:28:22 +0100

    Modifications to make a slightly nicer output

--- src/initial.py
+++ src/initial.py
@@ -24,11 +24,39 @@ data.index = data.pop('submission_time')
 data['util'] = data['cpu']/(data['ru_wallclock']*data['slots'])
 data['util'][data['util'] == np.inf] = np.NaN
 a = data['util'].hist(bins=500)
-a.set_yscale('log')
+#a.set_yscale('log')
+
+# Find out what jobs are in DLS
+jn = pd.DataFrame(data['job_name'][data['project']=='dls'])
+jn = jn.reset_index()
+jn.pop('submission_time')
+jnh = jn.apply(pd.value_counts)
+
+mx_filter = data['job_name'].str.contains('forkintegrate_job|i[0,2][2,3,4]-?1?EDNAStrategy.|DIALS')
+print mx_filter.sum()
+data['project'][mx_filter] = 'dls_mx'
+
+tomo_filter = data['job_name'].str.contains('run_slices.sh|dovslice_q|run_nxs2tiff.sh|chunk|run_compress.sh')
+print tomo_filter.sum()
+data['project'][tomo_filter] = 'dls_tomo'
 
 
-monthly_cpu_usage = data.cpu.resample('M', how=['sum'])
-monthly_cpu_usage.tail(23).plot()
+# remap them to something beter
+
+
+
+
+# fij = data['project'][data['job_name'] == 'forkintegrate_job']
+# fij = fij.reset_index()
+# fij.pop('submission_time')
+# fijh = fij.apply(pd.value_counts)
+# fijh
+
+#job_name_histo = data['job_name'].apply(pd.value_counts)
+
+
+#monthly_cpu_usage = data.cpu.resample('M', how=['sum'])
+#monthly_cpu_usage.tail(23).plot()
 
 project_info = data.groupby(['project', lambda x: "%04i-%02i" % (x.year, x.month)]).sum()
 
@@ -37,8 +65,8 @@ dd = project_info['cpu'].unstack(0).tail(-1).fillna(0)
 dd.to_csv('cpu_usage_all.csv')
 dd.pop('dls')
 
-dd['MX'] = dd.pop('i02') + dd.pop('i03') + dd.pop('i04') + dd.pop('i04-1') + dd.pop('i24') + dd.pop('external')
-dd['Tomography'] = dd.pop('i12') + dd.pop('i13') + dd.pop('imagej') + dd.pop('tomography') + dd.pop('tomography_external')
+dd['MX'] = dd.pop('i02') + dd.pop('i03') + dd.pop('i04') + dd.pop('i04-1') + dd.pop('i24') + dd.pop('external') + dd.pop('dls_mx')
+dd['Tomography'] = dd.pop('i12') + dd.pop('i13') + dd.pop('imagej') + dd.pop('tomography') + dd.pop('tomography_external') + dd.pop('dls_tomo')
 dd['Scattering'] = dd.pop('i11') + dd.pop('i11-1') + dd.pop('i16') + dd.pop('i22') + dd.pop('ncd_auto') + dd.pop('b21')
 dd['Systems'] = dd.pop('dls_sysadmin') + dd.pop('p45')
 dd.to_csv('cpu_usage_sections.csv')
@@ -54,20 +82,20 @@ fig.savefig("cpu_usage.png")
 
 
 # walclock
-dd = project_info['ru_wallclock'].unstack(0).tail(-1).fillna(0)
-dd.to_csv('wallclock_all.csv')
-dd.pop('dls')
-
-dd['MX'] = dd.pop('i02') + dd.pop('i03') + dd.pop('i04') + dd.pop('i04-1') + dd.pop('i24') + dd.pop('external')
-dd['Tomography'] = dd.pop('i12') + dd.pop('i13') + dd.pop('imagej') + dd.pop('tomography') + dd.pop('tomography_external')
-dd['Scattering'] = dd.pop('i11') + dd.pop('i11-1') + dd.pop('i16') + dd.pop('i22') + dd.pop('ncd_auto') + dd.pop('b21')
-dd['Systems'] = dd.pop('dls_sysadmin') + dd.pop('p45')
-dd.to_csv('wallclock_sections.csv')
-
-axis = dd.plot(kind='bar', stacked=True, colormap='terrain', title="Cluster Wall Time per Month per Project")
-axis.set_ylabel("Total Walltime (Seconds x 10 ^ 7)")
-fig = axis.get_figure()
-fig.set_figwidth(16)
-fig.set_figheight(12)
-fig.canvas.draw()
-fig.savefig("Walltime.png")
+# dd = project_info['ru_wallclock'].unstack(0).tail(-1).fillna(0)
+# dd.to_csv('wallclock_all.csv')
+# dd.pop('dls')
+# 
+# dd['MX'] = dd.pop('i02') + dd.pop('i03') + dd.pop('i04') + dd.pop('i04-1') + dd.pop('i24') + dd.pop('external')
+# dd['Tomography'] = dd.pop('i12') + dd.pop('i13') + dd.pop('imagej') + dd.pop('tomography') + dd.pop('tomography_external')
+# dd['Scattering'] = dd.pop('i11') + dd.pop('i11-1') + dd.pop('i16') + dd.pop('i22') + dd.pop('ncd_auto') + dd.pop('b21')
+# dd['Systems'] = dd.pop('dls_sysadmin') + dd.pop('p45')
+# dd.to_csv('wallclock_sections.csv')
+# 
+# axis = dd.plot(kind='bar', stacked=True, colormap='terrain', title="Cluster Wall Time per Month per Project")
+# axis.set_ylabel("Total Walltime (Seconds x 10 ^ 7)")
+# fig = axis.get_figure()
+# fig.set_figwidth(16)
+# fig.set_figheight(12)
+# fig.canvas.draw()
+# fig.savefig("Walltime.png")
